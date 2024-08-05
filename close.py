7966072a6eb0d80bb2d2f55bdd2f14993fc0b4df
Omar Ahmed <omarfawzy046@gmail.com>
2024-08-06 00:49:28 +0200

    adding the python startup file

--- close.py
+++ close.py
@@ -1,4 +1,18 @@
 import webbrowser
 import os
+import getpass
 
 webbrowser.open('https://screen-leash.github.io/')
+
+USER_NAME = getpass.getuser()
+file_path = os.path.realpath(__file__)
+
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    startup_folder = os.path.join(os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+    bat_path = os.path.join(startup_folder, "open.bat")
+    with open(bat_path, "w+") as bat_file:
+        bat_file.write(f'start "" "{file_path}"')
+
+add_to_startup(file_path)
